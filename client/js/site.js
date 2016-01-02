d175d10da5ab5cdf47753f216cb06d9128f29a26
Will Boyd <will@codersblock.com>
2016-01-02 15:54:15 -0500

    Concepts of window and cooldown

--- client/js/site.js
+++ client/js/site.js
@@ -1,31 +1,45 @@
 (function() {
-	var captureIntervalDelay = 100;
+	// config
+	var captureIntervalDelay = 100;	// time between captures, in ms
+	var captureWindow = 2000;		// time to consider best capture, in ms
+	var captureCooldown = 8000;		// time to chill after saving capture, in ms
 	var captureWidth = 320;
 	var captureHeight = 240;
 	var diffWidth = 32;
 	var diffHeight = 24;
-	var diffThreshold = 32;
-	var diffSpan = 2;
+	var pixelDiffThreshold = 32;	// min for a pixel to be considered different
+	var scoreThreshold = 4;			// min for an image to be considered different
 
-	var verdict, streamVideo, captureCanvas, captureContext, diffCanvas, diffContext, motionCanvas, motionContext;
-	var captureInterval;
-	var captures = [];
+	// shared
+	var isCaptureWindow;			// currently within a capture window?
+	var isCaptureCooldown;			// currently taking a break from capturing?
+	var captureInterval;			// set interval for capturing
+	var oldImage;					// image to compare captured images against
+	var bestDiff;					// best (most significant) diff during a capture window
+
+	// reused elements and canvas contexts
+	var verdict, streamVideo, captureCanvas, captureContext, diffCanvas, diffContext,
+		motionCanvas, motionContext;
 
 	function init() {
+		// cache some elements
 		verdict = document.getElementById('verdict');
 		streamVideo = document.getElementById('stream');
 
+		// create canvas for captures in memory
 		captureCanvas = document.createElement('canvas');
 		captureCanvas.width = captureWidth;
 		captureCanvas.height = captureHeight;
 		captureContext = captureCanvas.getContext('2d');
 
+		// create canvas for diffing in memory
 		diffCanvas = document.createElement('canvas');
 		diffCanvas.width = diffWidth;
 		diffCanvas.height = diffHeight;
 		diffContext = diffCanvas.getContext('2d');
 		diffContext.globalCompositeOperation = 'difference';
 
+		// set up canvas on page for showing motion
 		motionCanvas = document.getElementById('motion');
 		motionCanvas.width = diffWidth;
 		motionCanvas.height = diffHeight;
@@ -47,65 +61,106 @@
 
 	function startStreamingVideo(stream) {
 		streamVideo.srcObject = stream;
-		startCapturing();
+		captureInterval = self.setInterval(getCapture, captureIntervalDelay);
 	}
 
 	function displayError(error) {
 		console.log(error);
 	}
 
-	function startCapturing() {
-		captureInterval = self.setInterval(getCapture, captureIntervalDelay);
-	}
-
 	function getCapture() {
+		if (isCaptureCooldown) {
+			return;
+		}
+
+		// capture from video
 		captureContext.drawImage(streamVideo, 0, 0, captureWidth, captureHeight);
 
+		// save as image
 		var newImage = new Image();
 		newImage.onload = saveCapture;
-		newImage.src = captureCanvas.toDataURL('image/png');
+		newImage.src = captureCanvas.toDataURL();
 	}
 
 	function saveCapture() {
-		captures.unshift(this);
-		captures = captures.slice(0, diffSpan);
+		var newImage = this;
+		if (!oldImage) {
+			// don't have an old image to compare to, but next time we will
+			oldImage = newImage;
+			return;
+		}
 
-		var oldImage = captures[captures.length - 1];
-		var diff = checkDiff(oldImage, this);
-		verdict.textContent = diff.thresholdCount >= 5 ? 'yes' : 'no';
-		motionContext.putImageData(diff.imgData, 0, 0);
+		var diff = calculateDiff(oldImage, newImage);
+
+		// show motion on page
+		motionContext.putImageData(diff.diffImgData, 0, 0);
+
+		if (isCaptureWindow) {
+			if (diff.score > bestDiff.score) {
+				// this is our new best diff for this capture window
+				bestDiff = diff;
+			}
+		} else {
+			if (diff.score > scoreThreshold) {
+				// this capture is good enough to start a new capture window
+				bestDiff = diff;
+				isCaptureWindow = true;
+				setTimeout(commitBest, captureWindow);
+			} else {
+				// new capture not noteworthy
+				oldImage = newImage;
+			}
+		}
 	}
 
-	function checkDiff(oldImage, newImage) {
+	function calculateDiff(oldImage, newImage) {
+		// clear canvas and draw both images
 		diffContext.clearRect(0, 0, diffWidth, diffHeight);
 		diffContext.drawImage(oldImage, 0, 0, diffWidth, diffHeight);
 		diffContext.drawImage(newImage, 0, 0, diffWidth, diffHeight);
 
-		var imgData = diffContext.getImageData(0, 0, diffWidth, diffHeight);
-		var rgba = imgData.data;
+		var diffImgData = diffContext.getImageData(0, 0, diffWidth, diffHeight);
+		var rgba = diffImgData.data;
 
-		var thresholdCount = 0;
-		var diffAverage = 0;
+		// score each pixel, adjust color for display
+		var score = 0;
 		for (var i = 0; i < rgba.length; i += 4) {
-			var diff = rgba[i] * 0.3 + rgba[i + 1] * 0.6 + rgba[i + 2] * 0.1;
-			var lit = Math.min(255, diff * (255 / diffThreshold));
+			var pixelDiff = rgba[i] * 0.3 + rgba[i + 1] * 0.6 + rgba[i + 2] * 0.1;
+			var normalized = Math.min(255, pixelDiff * (255 / pixelDiffThreshold));
 			rgba[i] = 0;
-			rgba[i + 1] = lit;
+			rgba[i + 1] = normalized;
 			rgba[i + 2] = 0;
 
-			if (diff >= diffThreshold) {
-				thresholdCount++;
+			if (pixelDiff >= pixelDiffThreshold) {
+				score++;
 			}
-			diffAverage += diff;
 		}
-		diffAverage /= rgba.length / 4;
 
 		return {
-			imgData: imgData,
-			thresholdCount: thresholdCount,
-			diffAverage: diffAverage
+			oldImage: oldImage,
+			newimage: newImage,
+			diffImgData: diffImgData,
+			score: score
 		};
 	}
 
+	function commitBest() {
+		upload();
+		isCaptureWindow = false;
+
+		oldImage = undefined;
+		bestDiff = undefined;
+
+		isCaptureCooldown = true;
+		setTimeout(function() {
+			isCaptureCooldown = false;
+		}, captureCooldown);
+	}
+
+	function upload() {
+		console.log('This is when uploading would happen...');
+	}
+
+	// kick things off
 	init();
 })();
