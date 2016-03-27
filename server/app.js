b43f1c193b1d72212020157869a68f660943850b
Will Boyd <will@codersblock.com>
2016-03-27 11:17:06 -0400

    Fixed env var loading

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 
-if (!process.env.ENVIRONMENT) {
-	require('../../env.js');
+if (process.env.ENVIRONMENT !== 'prod') {
+	try {
+		// attempt to load dev .env file
+		require('../../.env');
+	} catch (e) {
+		throw 'Environment variables not found. If in dev, make sure your .env file is in place. If in prod, make sure you have properly configured your environment variables.';
+	}
 }
 
 var app = express();
